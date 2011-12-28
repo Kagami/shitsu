92e609a332adf173941410d56d21cdbdff6318eb
Kagami Hiiragi <kagami@genshiken.org>
2011-12-29 00:50:32 +0400

    refactor bot class

--- C.C..py
+++ C.C..py
@@ -1,5 +1,26 @@
+#!/usr/bin/env python
+
+###############################################
+# C.C. - jabber bot
+# Copyright (C) 2008 anonymous
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################
+
 import os
 import sys
+os.chdir(sys.path[0])
 import imp
 import time
 import signal
@@ -17,9 +38,10 @@ except ImportError:
     import xmpp
 import misc
 sys.path.insert(0, 'modules')
+import config
 
 
-class JabberBot(object):
+class CC(object):
 
     reconnectTime = 30
 
@@ -334,3 +356,8 @@ class ReloadData(Exception):
 
 def sigHupCB(signum, frame):
     raise ReloadData()
+
+
+cc = CC(config.user, config.rooms, config.owner)
+cc.load()
+cc.process()
